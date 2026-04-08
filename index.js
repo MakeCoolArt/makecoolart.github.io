718888e59b6ffeb8fdaee4e2eabab628a51842cc
YTRellVr <R4llalt@gmail.com>
2026-04-08 00:24:39 -0400

    Change server port from 3000 to 1000

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.post('/photon-webhook', (req, res) => {
 });
 
 // Use Render's dynamic port or default to 3000
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 1000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
